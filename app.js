d235a72db2b3aef5b911e483db1ec5edc4c5b016
Abdo Hatem <abdulrahmanhatem428@gmail.com>
2025-09-22 00:41:23 +0300

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRoute = require("./routes/userRoute");
 const reviewRoute = require("./routes/reviewRoute");
+const appointmentroute = require("./routes/appointmentroute")
 const globalErrorHandler = require("./controllers/errorController");
 const app = express();
 
@@ -9,6 +10,6 @@ app.use(express.json());
 // add versions for future updates
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/reviews", reviewRoute);
-
+app.use("/api/v1/appointments" , appointmentroute)
 app.use(globalErrorHandler);
 module.exports = app;
--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -1,28 +1,28 @@
 const mongoose = require("mongoose");
 const { Schema, model, Types } = mongoose;
-
-const AppointmentSchema = new Schema(
-  {
-    doctor: {
-      type: Types.ObjectId,
-      ref: "Doctor",
-      required: [true, "Doctor is required"],
-    },
-    patient: {
-      type: Types.ObjectId,
-      ref: "Patient",
-      required: [true, "Patient is required"],
-    },
-    bookingDate: { type: Date, required: [true, "Booking date is required"] },
-    status: {
-      type: String,
-      enum: ["booked", "cancelled", "completed"],
-      default: "booked",
-    },
-    notes: { type: String, trim: true },
+const appointmentSchema = new Schema({
+  doctorId: {
+    type: Types.ObjectId,
+    ref: "Doctor",
+    required: [true, "DoctorId is required"],
   },
-  { timestamps: true }
-);
+  patientId: {
+    type: Types.ObjectId,
+    ref: "Patient",
+    required: [true, "PatientId is required"],
+  },
+  bookingDate: {
+    type: Date,
+    required: [true, "Booking date is required"],
+  },
+  status: {
+    type: String,
+    enum: ["booked", "cancelled", "completed"],
+    default: "booked",
+  },
+  notes: { type: String, trim: true },
+} , {timestamps : true});
+appointmentSchema.index({doctorId : 1 , bookingDate : 1} , {unique : true})
+const Appointment = model ("Appointment" , appointmentSchema)
+module.exports = Appointment
 
-const Appointment = model("Appointment", AppointmentSchema);
-module.exports = Appointment;
